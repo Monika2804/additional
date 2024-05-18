2a3943731df1f0aed6e75cc30e257ae248ebb161
Monika Bade <monika.bade04@gmail.com>
2024-05-18 23:34:59 +0530

    Add files via upload
    language - c
    Dev c++
    all arithmetic operation perform given  input by user.

---
+++ Addition.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	float b;
+	printf("Enter number a: ");
+	scanf("%d",&a);
+	printf("Enter number b: ");
+	scanf("%f",&b);
+	printf("\nsum of a+b = %f",a+b);
+	printf("\nsum of a-b = %f",a-b);
+	printf("\nsum of a*b = %f",a*b);
+	printf("\nsum of a+b = %f",a+b);
+	printf("\nsum of a/b = %f",a/b);
+	return 0;
+}
